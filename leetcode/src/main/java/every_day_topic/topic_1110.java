876d9009d155aa8a94192d4f4cb0b3686b8b3aa9
zhaoxuan <zhaoxuan@sinesegye.com.cn>
2023-05-30 13:56:01 +0800

    [Feat] topic_1110

---
+++ leetcode/src/main/java/every_day_topic/topic_1110.java
@@ -0,0 +1,51 @@
+package every_day_topic;
+
+import data_structure.TreeNode;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@SuppressWarnings("unused")
+public class topic_1110 {
+
+    public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
+
+        List<TreeNode> result = new ArrayList<>();
+
+        Set<Integer> toDelete = Arrays.stream(to_delete).boxed().collect(Collectors.toSet());
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        if (!toDelete.contains(root.val)) {
+            result.add(root);
+        }
+
+        while (!queue.isEmpty()) {
+
+            TreeNode curr = queue.poll();
+
+            if (curr.left != null)
+                queue.offer(curr.left);
+
+            if (curr.right != null)
+                queue.offer(curr.right);
+
+            if (toDelete.contains(curr.val)) {
+                if (curr.right != null && !toDelete.contains(curr.right.val))
+                    result.add(curr.right);
+                if (curr.left != null && !toDelete.contains(curr.left.val))
+                    result.add(curr.left);
+            }
+
+            if (curr.right != null && toDelete.contains(curr.right.val))
+                curr.right = null;
+            if (curr.left != null && toDelete.contains(curr.left.val))
+                curr.left = null;
+        }
+
+        return result;
+
+    }
+
+}
