3e923b028358a52351e20ae373c3bab4fe653b47
zhaoxuan <zhaoxuan@sinesegye.com.cn>
2023-06-29 20:22:57 +0800

    [Feat] topic_0015

--- leetcode/src/main/java/every_day_topic/topic_0015.java
+++ leetcode/src/main/java/every_day_topic/topic_0015.java
@@ -28,4 +28,47 @@ public class topic_0015 {
         }
         return new ArrayList<>(result);
     }
+
+
+    List<List<Integer>> result = null;
+
+    public List<List<Integer>> batterThreeSum(int[] nums) {
+
+        return new AbstractList<List<Integer>>() {
+            public List<Integer> get(int index) {
+                init();
+                return result.get(index);
+            }
+
+            public int size() {
+                init();
+                return result.size();
+            }
+
+            private void init() {
+                if (result != null) return;
+
+                final int n = nums.length;
+                final Set<List<Integer>> set = new HashSet<List<Integer>>();
+                Arrays.sort(nums);//进行排序
+                for (int start = 0; start <= n - 3; start++) {
+                    int firstNum = nums[start];
+                    int mid = start + 1;
+                    int end = n - 1;
+                    while (mid < end) {
+                        if (firstNum + nums[mid] + nums[end] == 0) {
+                            set.add(Arrays.asList(firstNum, nums[mid], nums[end]));
+                            mid++;
+                            end--;
+                        } else if (firstNum + nums[mid] + nums[end] < 0) mid++;
+                        else end--;
+                    }
+                }
+
+                result = new ArrayList<List<Integer>>(set);
+
+            }
+        };
+    }
+
 }
