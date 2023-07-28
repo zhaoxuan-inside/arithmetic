88c92b3d25428d110b06f5e824e26fb0a86ebdb0
zhaoxuan <zhaoxuanmail@163.com>
2023-07-28 13:06:54 +0800

    [Feat] topic_2500

--- leetcode/src/main/java/every_day_topic/topic_2500.java
+++ leetcode/src/main/java/every_day_topic/topic_2500.java
@@ -1,2 +1,30 @@
-package every_day_topic;public class topic_2500 {
+package every_day_topic;
+
+import java.util.Arrays;
+
+@SuppressWarnings("unused")
+public class topic_2500 {
+    public int deleteGreatestValue(int[][] grid) {
+
+        int result = 0;
+
+        for (int[] ints : grid) {
+            Arrays.sort(ints);
+        }
+
+        for (int col = 0; col < grid[0].length; col++) {
+
+            int max = grid[0][col];
+
+            for (int row = 1; row < grid.length; row++) {
+                max = Math.max(max, grid[row][col]);
+            }
+
+            result += max;
+
+        }
+
+        return result;
+
+    }
 }
