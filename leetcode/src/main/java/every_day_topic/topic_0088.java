fea8c30dd0b0319f1bb3555faaf2ab8ef102e724
zhaoxuan-inside <zhaoxuanmail@163.com>
2023-08-14 13:54:39 +0800

    [Feat] topic_0088

---
+++ leetcode/src/main/java/every_day_topic/topic_0088.java
@@ -0,0 +1,58 @@
+package every_day_topic;
+
+@SuppressWarnings("unused")
+public class topic_0088 {
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        if (m == 0) {
+            if (n >= 0) {
+                System.arraycopy(nums2, 0, nums1, 0, n);
+            }
+            return;
+        }
+
+        int[] copyNums1 = new int[m];
+
+        System.arraycopy(nums1, 0, copyNums1, 0, m);
+
+        int index1 = 0;
+        int index2 = 0;
+        int index;
+        for (index = 0; index < m + n; index++) {
+
+            if (index1 >= m || index2 >= n) {
+                break;
+            }
+
+            if (copyNums1[index1] < nums2[index2]) {
+                nums1[index] = copyNums1[index1];
+                index1++;
+            } else {
+                nums1[index] = nums2[index2];
+                index2++;
+            }
+        }
+
+        if (index1 < m) {
+            for (int indexRemain = index1; indexRemain < m; indexRemain++) {
+                nums1[index] = copyNums1[indexRemain];
+                index++;
+            }
+            return;
+        }
+
+        for (int indexRemain = index2; indexRemain < n; indexRemain++) {
+            nums1[index] = nums2[indexRemain];
+            index++;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = new int[]{4,5,6,0,0,0};
+        int[] nums2 = new int[]{1,2,3};
+        merge(nums1, 3, nums2, 3);
+        for (int i : nums1) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ leetcode/src/main/java/every_day_topic/topic_0617.java
@@ -0,0 +1,14 @@
+package every_day_topic;
+
+import data_structure.TreeNode;
+
+public class topic_0617 {
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        TreeNode result = new TreeNode();
+
+
+
+
+        return result;
+    }
+}
