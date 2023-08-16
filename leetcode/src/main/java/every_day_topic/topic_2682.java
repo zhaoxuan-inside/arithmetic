0330425f66c9b16d66e034fc6340054c2f718ae5
zhaoxuan-inside <zhaoxuanmail@163.com>
2023-08-16 13:54:32 +0800

    [Feat] topic_2682

---
+++ leetcode/src/main/java/every_day_topic/topic_2682.java
@@ -0,0 +1,32 @@
+package every_day_topic;
+
+public class topic_2682 {
+    public static int[] circularGameLosers(int n, int k) {
+        boolean[] bool = new boolean[n];
+        int remain = n;
+
+        int index = 0;
+        int count = 0;
+        while (!bool[index]) {
+            bool[index] = true;
+            remain--;
+            index = (index + ++count * k) % n;
+        }
+
+        int[] result = new int[remain];
+        index = 0;
+        for (int i = 0; i < n; i++) {
+            if (!bool[i]) {
+                result[index++] = i + 1;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        for (int i : circularGameLosers(4, 4)) {
+            System.out.println(i);
+        }
+    }
+}
