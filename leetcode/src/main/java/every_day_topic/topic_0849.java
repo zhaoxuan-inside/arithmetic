80f9e7b0a60c49d31fa4d73e6d5acc2dcaeed7c3
zhaoxuan-inside <zhaoxuanmail@163.com>
2023-08-22 13:26:59 +0800

    [Feat] topic_0849

---
+++ leetcode/src/main/java/every_day_topic/topic_0849.java
@@ -0,0 +1,38 @@
+package every_day_topic;
+
+public class topic_0849 {
+    public static int maxDistToClosest(int[] seats) {
+        int max = 0;
+
+        int pre = 0;
+        int last = 0;
+        int length = seats.length;
+
+        while (last <= length - 1) {
+            if (seats[last] == 0) {
+                ++last;
+                if (last < length - 1) {
+                    continue;
+                }
+                if (last >= length) {
+                    return max;
+                }
+            }
+
+            int gap = last - pre;
+            if ((last < length - 1 && seats[pre] == 1) || (seats[last] == 1 && seats[pre] == 1)) {
+                gap = gap / 2;
+            }
+            max = Math.max(gap, max);
+            pre = last;
+            last++;
+        }
+
+        return max;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxDistToClosest(new int[]{0, 1}));
+    }
+}
