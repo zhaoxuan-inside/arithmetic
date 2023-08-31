15b64417cb8fdfbd3db9966061009ebea09ee6a5
zhaoxuan-inside <zhaoxuanmail@163.com>
2023-08-31 13:45:17 +0800

    [Feat] topic_0056

---
+++ leetcode/src/main/java/every_day_topic/topic_0056.java
@@ -0,0 +1,31 @@
+package every_day_topic;
+
+import java.util.Arrays;
+
+public class topic_0056 {
+    public static int[][] merge(int[][] intervals) {
+
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+        int curr = 0;
+        int scan = 1;
+
+        while (scan < intervals.length) {
+            if (Math.max(intervals[curr][0], intervals[scan][0]) <= Math.min(intervals[curr][1], intervals[scan][1])) {
+                intervals[curr][0] = Math.min(intervals[curr][0], intervals[scan][0]);
+                intervals[curr][1] = Math.max(intervals[curr][1], intervals[scan][1]);
+            } else {
+                curr++;
+                intervals[curr] = intervals[scan];
+            }
+            scan++;
+        }
+
+        return Arrays.copyOfRange(intervals, 0, curr + 1);
+
+    }
+
+    public static void main(String[] args) {
+        merge(new int[][]{{2, 3}, {4, 5}, {6, 7}, {8, 9}, {1, 10}});
+    }
+}
