9e572cb5574822d5afae96feed123dc8042cb7c7
zhaoxuan-inside <zhaoxuanmail@163.com>
2023-09-02 20:56:37 +0800

    [Feat] topic_2446

---
+++ leetcode/src/main/java/every_day_topic/topic_2446.java
@@ -0,0 +1,18 @@
+package every_day_topic;
+
+public class topic_2446 {
+    public static boolean haveConflict(String[] event1, String[] event2) {
+
+        int event1Start = Integer.parseInt(event1[0].replace(":", ""));
+        int event1End = Integer.parseInt(event1[1].replace(":", ""));
+        int event2Start = Integer.parseInt(event2[0].replace(":", ""));
+        int event2End = Integer.parseInt(event2[1].replace(":", ""));
+
+        return Math.max(event1Start, event2Start) <= Math.min(event1End, event2End);
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(haveConflict(new String[]{"01:15", "02:00"}, new String[]{"02:00", "03:00"}));
+    }
+}
