fb813e54b30d966a746d73afbc0fbbc7fb8ab0a2
zhaoxuan-inside <zhaoxuanmail@163.com>
2023-09-03 14:59:47 +0800

    [Feat] topic_1921

---
+++ leetcode/src/main/java/every_day_topic/topic_1921.java
@@ -0,0 +1,29 @@
+package every_day_topic;
+
+public class topic_1921 {
+
+    public static int eliminateMaximum(int[] dist, int[] speed) {
+        double[] time = new double[dist.length];
+        for (int index = 0; index < dist.length; index++) {
+            time[index] = (double) dist[index] / speed[index];
+        }
+
+        for (int fireCount = 0; fireCount < dist.length; fireCount++) {
+            int count = 0;
+            for (int index = 0; index < dist.length; index++) {
+                if (fireCount >= time[index]) {
+                    count++;
+                }
+            }
+            if (fireCount < count) {
+                return fireCount;
+            }
+        }
+        return dist.length;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(eliminateMaximum(new int[]{1,1,2,3}, new int[]{1,1,1,1}));
+    }
+
+}
