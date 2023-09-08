498086b86fc222bf9f3a7b338768b7891464b5fa
zhaoxuan-inside <zhaoxuanmail@163.com>
2023-09-08 19:56:10 +0800

    [Feat] topic_2409

---
+++ leetcode/src/main/java/every_day_topic/topic_2418.java
@@ -0,0 +1,24 @@
+package every_day_topic;
+
+import java.util.*;
+
+public class topic_2418 {
+    public String[] sortPeople(String[] names, int[] heights) {
+
+        Map<Integer, String> map = new HashMap<>();
+
+        for (int index = 0; index < names.length; index++) {
+            map.put(heights[index], names[index]);
+        }
+
+        Arrays.sort(heights);
+
+        String[] result = new String[names.length];
+        for (int index = 0; index < names.length; index++) {
+            result[index] = map.get(heights[names.length - index]);
+        }
+
+        return result;
+
+    }
+}
