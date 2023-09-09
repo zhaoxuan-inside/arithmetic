b06c1dc41431b1dac409f397b2a8e936f9b25169
zhaoxuan-inside <zhaoxuanmail@163.com>
2023-09-09 16:05:01 +0800

    [Feat] topic_1137

---
+++ leetcode/src/main/java/every_day_topic/topic_1137.java
@@ -0,0 +1,51 @@
+package every_day_topic;
+
+public class topic_1137 {
+
+    private static final int num = (int) Math.pow(2, 32) - 1;
+
+    public static int tribonacci(int n) {
+
+        if (n < 4) {
+            switch (n) {
+                case 0 :
+                    return 0;
+                case 1 :
+                    return 1;
+                case 2:
+                    return 1;
+            }
+        }
+
+        int pre = 0;
+        int mid = 1;
+        int last = 1;
+        int tmp;
+
+        for (int i = 2; i < n; i++) {
+            tmp = pre + mid + last;
+            pre = mid;
+            mid = last;
+            last = tmp;
+        }
+        return last;
+    }
+
+    public static int batter(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1 | n == 2) {
+            return 1;
+        }
+
+        return batter(n - 1) + batter(n - 2) + batter(n - 3);
+
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(batter(25));
+    }
+
+}
