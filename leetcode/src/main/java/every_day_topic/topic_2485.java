ac3370ac751c4b75e934609009739d8a381901ae
zhaoxuan-inside <zhaoxuanmail@163.com>
2023-09-16 21:05:52 +0800

    [Feat] topic_2485

---
+++ leetcode/src/main/java/every_day_topic/topic_2485.java
@@ -0,0 +1,26 @@
+package every_day_topic;
+
+public class topic_2485 {
+    public int pivotInteger(int n) {
+
+        int left = 0;
+        int right = 0;
+        int leftIndex = 1;
+        int rightIndex = n;
+
+        while (leftIndex < rightIndex) {
+            if (left < right) {
+                left += leftIndex++;
+            } else {
+                right += rightIndex--;
+            }
+        }
+
+        if (left == right) {
+            return left;
+        } else {
+            return -1;
+        }
+
+    }
+}
