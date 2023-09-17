83242d69987f819c5ebe99f87704dafb0b59436c
zhaoxuan-inside <zhaoxuanmail@163.com>
2023-09-17 22:41:30 +0800

    [Feat] topic_0101

---
+++ leetcode/src/main/java/every_day_topic/topic_0101.java
@@ -0,0 +1,38 @@
+package every_day_topic;
+
+@SuppressWarnings("unused")
+public class topic_0101 {
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        return check(root, root);
+    }
+
+    public boolean check(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        return p.val == q.val && check(p.left, q.right) && check(p.right, q.left);
+    }
+}
