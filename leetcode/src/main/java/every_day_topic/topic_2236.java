0da5135b5a350e04a5a5735ff67509528eea87f4
zhaoxuan-inside <zhaoxuanmail@163.com>
2023-09-17 21:45:36 +0800

    [Feat] topic_2236

--- leetcode/src/main/java/every_day_topic/topic_2236.java
+++ leetcode/src/main/java/every_day_topic/topic_2236.java
@@ -1,26 +1,29 @@
 package every_day_topic;
 
+@SuppressWarnings("unused")
 public class topic_2236 {
+    @SuppressWarnings("unused")
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
     public boolean checkTree(TreeNode root) {
         return root.val == root.left.val + root.right.val;
     }
-}
 
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
 }
