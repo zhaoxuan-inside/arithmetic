371779dd9b3a4878993b8123bf3a33bca98da1cc
zhaoxuan-inside <zhaoxuanmail@163.com>
2023-09-17 22:56:42 +0800

    [Feat] topic_0101

---
+++ leetcode/src/main/java/every_day_topic/topic_0104.java
@@ -0,0 +1,37 @@
+package every_day_topic;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+@SuppressWarnings("unused")
+public class topic_0104 {
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            int leftHeight = maxDepth(root.left);
+            int rightHeight = maxDepth(root.right);
+            return Math.max(leftHeight, rightHeight) + 1;
+        }
+    }
+}
