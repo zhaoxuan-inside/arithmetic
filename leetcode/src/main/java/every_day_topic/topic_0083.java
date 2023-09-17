812fd2e45b9a2fff30b9c012c92de226e0cc01ed
zhaoxuan-inside <zhaoxuanmail@163.com>
2023-09-17 22:03:06 +0800

    [Feat] topic_0083

---
+++ leetcode/src/main/java/every_day_topic/topic_0083.java
@@ -0,0 +1,38 @@
+package every_day_topic;
+
+@SuppressWarnings("unused")
+public class topic_0083 {
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    class Solution {
+        public ListNode deleteDuplicates(ListNode head) {
+            ListNode curr = head;
+            while (curr != null && curr.next != null) {
+                ListNode pre = curr.next;
+                if (curr.val == pre.val) {
+                    curr.next = pre.next;
+                } else {
+                    curr = pre;
+                }
+            }
+            return head;
+        }
+    }
+
+}
