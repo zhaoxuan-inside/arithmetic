10fb38e311456cc6418db6c4f9139aa802e46b82
zhaoxuan-inside <zhaoxuanmail@163.com>
2023-09-18 19:50:28 +0800

    [Feat] topic_0198

---
+++ leetcode/src/main/java/every_day_topic/topic_0198.java
@@ -0,0 +1,21 @@
+package every_day_topic;
+
+public class topic_0198 {
+    public static int rob(int[] nums) {
+        int[] mem = new int[nums.length];
+        mem[0] = nums[0];
+        int max = mem[0];
+
+        for (int index = 1; index < nums.length; index++) {
+            mem[index] = nums[index] + Math.max(index - 2 >= 0 ? mem[index - 2] : 0, index - 3 >= 0 ? mem[index - 3] : 0);
+            max = Math.max(max, mem[index]);
+        }
+
+        return max;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(rob(new int[]{1, 2}));
+    }
+}
