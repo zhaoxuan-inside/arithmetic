865ce6d4d64eb4e1824e4746b02c27a6f3e55256
zhaoxuan-inside <zhaoxuanmail@163.com>
2023-09-27 12:19:36 +0800

    [Feat] topic_0028

---
+++ leetcode/src/main/java/every_day_topic/topic_0028.java
@@ -0,0 +1,33 @@
+package every_day_topic;
+
+@SuppressWarnings("unused")
+public class topic_0028 {
+    public static int strStr(String haystack, String needle) {
+        int hayIndex;
+        int needleIndex = 0;
+
+        for (hayIndex = 0; hayIndex < haystack.length() - needle.length() + 1; ) {
+
+            while (needleIndex < needle.length() && haystack.charAt(hayIndex) == needle.charAt(needleIndex)) {
+                hayIndex++;
+                needleIndex++;
+            }
+
+            if (needleIndex == needle.length()) {
+                return hayIndex - needleIndex;
+            } else {
+                hayIndex = hayIndex - needleIndex + 1;
+                needleIndex = 0;
+            }
+
+        }
+
+        return -1;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(strStr("leetcode", "leeto"));
+    }
+
+}
