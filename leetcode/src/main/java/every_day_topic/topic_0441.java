0904b16db1b0ecfa1e32d8a7932f50371e9c9fcf
zhaoxuan-inside <zhaoxuanmail@163.com>
2023-09-27 12:37:52 +0800

    [Feat] topic_0441

---
+++ leetcode/src/main/java/every_day_topic/topic_0441.java
@@ -0,0 +1,20 @@
+package every_day_topic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SuppressWarnings("unused")
+public class topic_0441 {
+
+    public List<String> readBinaryWatch(int turnedOn) {
+        List<String> ans = new ArrayList<>();
+        for (int hour = 0; hour < 12; ++hour) {
+            for (int minute = 0; minute < 60; ++minute) {
+                if (Integer.bitCount(hour) + Integer.bitCount(minute) == turnedOn) {
+                    ans.add(hour + ":" + (minute < 10 ? "0" : "") + minute);
+                }
+            }
+        }
+        return ans;
+    }
+}
