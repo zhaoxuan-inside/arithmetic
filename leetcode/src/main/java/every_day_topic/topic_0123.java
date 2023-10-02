fd9aae3b68474d97445f0b6f56915f5e53e7bf44
zhaoxuan-inside <zhaoxuanmail@163.com>
2023-10-02 15:10:14 +0800

    [Feat] topic_0123

---
+++ leetcode/src/main/java/every_day_topic/topic_0123.java
@@ -0,0 +1,46 @@
+package every_day_topic;
+
+@SuppressWarnings("unused")
+public class topic_0123 {
+    public static int maxProfit(int[] prices) {
+        if (prices.length < 2) {
+            return 0;
+        }
+        int ans = 0;
+        for (int index = 0; index < prices.length; index++) {
+            ans = Math.max(ans, subMaxProfit(prices, 0, index) + subMaxProfit(prices, index, prices.length));
+        }
+        return ans;
+    }
+
+    private static int subMaxProfit(int[] prices, int start, int end) {
+        int ans = 0;
+
+        for (int endIndex = start; endIndex < end; endIndex++) {
+            int curr = 0;
+            for (int startIndex = endIndex - 1; startIndex >= start; startIndex--) {
+                if (prices[endIndex] <= prices[startIndex]) {
+                    break;
+                }
+                curr = Math.max(curr, prices[endIndex] - prices[startIndex]);
+            }
+            ans = Math.max(ans, curr);
+        }
+
+        return ans;
+    }
+
+    public int faster(int[] prices) {
+        int length = prices.length;
+        int buy1 = -prices[0], sell1 = 0;
+        int buy2 = -prices[0], sell2 = 0;
+        for (int index = 1; index < length; ++index) {
+            buy1 = Math.max(buy1, -prices[index]);
+            sell1 = Math.max(sell1, buy1 + prices[index]);
+            buy2 = Math.max(buy2, sell1 - prices[index]);
+            sell2 = Math.max(sell2, buy2 + prices[index]);
+        }
+        return sell2;
+    }
+
+}
