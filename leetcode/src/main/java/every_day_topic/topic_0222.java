8c97ee33af01c153ca5ee9560703479f076e738c
zhaoxuan-inside <zhaoxuanmail@163.com>
2023-10-02 18:57:17 +0800

    [Feat] topic_0222

--- leetcode/src/main/java/every_day_topic/topic_0222.java
+++ leetcode/src/main/java/every_day_topic/topic_0222.java
@@ -2,9 +2,36 @@ package every_day_topic;
 
 import data_structure.TreeNode;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 @SuppressWarnings("unused")
 public class topic_0222 {
     public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
 
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+        int ans = 1;
+
+        while (!queue.isEmpty()) {
+            Queue<TreeNode> tmp = new ArrayDeque<>();
+            while (!queue.isEmpty()) {
+                TreeNode poll = queue.poll();
+                if (poll.left != null) {
+                    tmp.offer(poll.left);
+                    ans++;
+                }
+                if (poll.right != null) {
+                    tmp.offer(poll.right);
+                    ans++;
+                }
+            }
+            queue.addAll(tmp);
+        }
+
+        return ans;
     }
 }
