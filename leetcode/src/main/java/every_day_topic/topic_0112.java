5e2cf502244fc67bf9cbad3d860541c8ffcad562
zhaoxuan-inside <zhaoxuanmail@163.com>
2023-10-02 19:41:32 +0800

    [Feat] topic_0112

---
+++ leetcode/src/main/java/every_day_topic/topic_0112.java
@@ -0,0 +1,19 @@
+package every_day_topic;
+
+import data_structure.TreeNode;
+
+@SuppressWarnings("unused")
+public class topic_0112 {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+
+        if (root.left == null && root.right == null) {
+            return root.val == targetSum;
+        }
+
+        return hasPathSum(root.left, targetSum - root.val) ||
+                hasPathSum(root.right, targetSum - root.val);
+    }
+}
