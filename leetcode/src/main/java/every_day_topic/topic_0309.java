ec0ac974ab8add7acaa62320342fbc41112b6dc8
zhaoxuan-inside <zhaoxuanmail@163.com>
2023-10-05 15:19:11 +0800

    [Feat] topic_0309

--- leetcode/src/main/java/every_day_topic/topic_0309.java
+++ leetcode/src/main/java/every_day_topic/topic_0309.java
@@ -13,6 +13,9 @@ public class topic_0309 {
         mem[0][0] = -prices[0];
 
         for (int index = 1; index < length; ++index) {
+            // f[i][0]: 手上持有股票的最大收益
+            // f[i][1]: 手上不持有股票，并且处于冷冻期中的累计最大收益
+            // f[i][2]: 手上不持有股票，并且不在冷冻期中的累计最大收益
             mem[index][0] = Math.max(mem[index - 1][0], mem[index - 1][2] - prices[index]);
             mem[index][1] = mem[index - 1][0] + prices[index];
             mem[index][2] = Math.max(mem[index - 1][1], mem[index - 1][2]);
