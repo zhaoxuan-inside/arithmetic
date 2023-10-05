41962db0df5c3240d1a9b1cce4dbd632c3783a08
zhaoxuan-inside <zhaoxuanmail@163.com>
2023-10-05 15:17:38 +0800

    [Feat] topic_0064

--- leetcode/src/main/java/every_day_topic/topic_0309.java
+++ leetcode/src/main/java/every_day_topic/topic_0309.java
@@ -3,21 +3,20 @@ package every_day_topic;
 @SuppressWarnings("unused")
 public class topic_0309 {
     public int maxProfit(int[] prices) {
-        if (prices.length == 0) {
+
+        if (prices.length <= 1) {
             return 0;
         }
 
-        int n = prices.length;
-        // f[i][0]: 手上持有股票的最大收益
-        // f[i][1]: 手上不持有股票，并且处于冷冻期中的累计最大收益
-        // f[i][2]: 手上不持有股票，并且不在冷冻期中的累计最大收益
-        int[][] f = new int[n][3];
-        f[0][0] = -prices[0];
-        for (int i = 1; i < n; ++i) {
-            f[i][0] = Math.max(f[i - 1][0], f[i - 1][2] - prices[i]);
-            f[i][1] = f[i - 1][0] + prices[i];
-            f[i][2] = Math.max(f[i - 1][1], f[i - 1][2]);
+        int length = prices.length;
+        int[][] mem = new int[length][3];
+        mem[0][0] = -prices[0];
+
+        for (int index = 1; index < length; ++index) {
+            mem[index][0] = Math.max(mem[index - 1][0], mem[index - 1][2] - prices[index]);
+            mem[index][1] = mem[index - 1][0] + prices[index];
+            mem[index][2] = Math.max(mem[index - 1][1], mem[index - 1][2]);
         }
-        return Math.max(f[n - 1][1], f[n - 1][2]);
+        return Math.max(mem[length - 1][1], mem[length - 1][2]);
     }
 }
