23d3c63c4fe54a75cf568a9e039979e39f983c2c
zhaoxuan-inside <zhaoxuanmail@163.com>
2023-10-12 13:47:03 +0800

    [Feat] topic_0073

--- leetcode/src/main/java/every_day_topic/topic_0073.java
+++ leetcode/src/main/java/every_day_topic/topic_0073.java
@@ -1,47 +1,36 @@
 package every_day_topic;
 
-import java.util.HashSet;
-import java.util.Set;
-
 @SuppressWarnings("unused")
 public class topic_0073 {
 
     public static void setZeroes(int[][] matrix) {
+        int rowCount = matrix.length;
+        int colCount = matrix[0].length;
 
-        Set<Integer> rows = new HashSet<>();
-        Set<Integer> cols = new HashSet<>();
+        int nodeCount = rowCount * colCount;
+        int[][] zeros = new int[nodeCount][2];
+        int zeroNodeCount = 0;
 
-        for (int row = 0; row < matrix.length; row++) {
-            if (rows.contains(row)) {
-                continue;
-            }
-            for (int col = 0; col < matrix[0].length; col++) {
-                if (cols.contains(col)) {
-                    continue;
-                }
+        for (int row = 0; row < rowCount; row++) {
+            for (int col = 0; col < colCount; col++) {
                 if (matrix[row][col] == 0) {
-                    setZero(matrix, row, col);
-                    rows.add(row);
-                    cols.add(col);
-                    break;
+                    zeros[zeroNodeCount][0] = row;
+                    zeros[zeroNodeCount][1] = col;
+                    zeroNodeCount++;
                 }
             }
         }
 
-    }
-
-    private static void setZero(int[][] matrix, int row, int col) {
-
-        for (int i = 0; i < matrix.length; i++) {
-            matrix[i][col] = 0;
+        for (int index = 0; index < zeroNodeCount; index++) {
+            int row = zeros[index][0];
+            int col = zeros[index][1];
+            for (int rowIdx = 0; rowIdx < rowCount; rowIdx++) {
+                matrix[rowIdx][col] = 0;
+            }
+            for (int colIdx = 0; colIdx < colCount; colIdx++) {
+                matrix[row][colIdx] = 0;
+            }
         }
-
-        for (int i = 0; i < matrix[0].length; i++) {
-
-            matrix[row][i] = 0;
-
-        }
-
     }
 
 }
