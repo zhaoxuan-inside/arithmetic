a64eb739644f9ea95af2f883eb2231d50636a2e3
zhaoxuan-inside <zhaoxuanmail@163.com>
2023-10-16 16:29:26 +0800

    [Feat] 竞赛

---
+++ leetcode/src/main/java/every_day_topic/compete_100077.java
@@ -0,0 +1,52 @@
+package every_day_topic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SuppressWarnings("unused")
+public class compete_100077 {
+    private static int[] wordLens;
+
+    public static List<String> getWordsInLongestSubsequence(int n, String[] words, int[] groups) {
+        wordLens = new int[n];
+        for (int index = 0; index < n; index++) {
+            wordLens[index] = words[index].length();
+        }
+
+        List<Integer> sub = findSub(groups, 0);
+        List<String> ans = new ArrayList<>();
+        for (Integer index : sub) {
+            ans.add(words[index]);
+        }
+        return ans;
+
+    }
+
+    private static List<Integer> findSub(int[] group, int startIndex) {
+
+        List<Integer> indexList = new ArrayList<>();
+        List<Integer> otherIndexList = new ArrayList<>();
+
+        indexList.add(0);
+        int last = group[0];
+        for (int index = startIndex + 1; index < group.length; index++) {
+            if (last != group[index] &&
+                    group[index] < group.length &&
+                    wordLens[last] == wordLens[group[index]]) {
+                indexList.add(index);
+                last = group[index];
+            } else {
+                otherIndexList = findSub(group, index);
+            }
+        }
+
+        return indexList.size() > otherIndexList.size() ? indexList : otherIndexList;
+
+    }
+
+    public static void main(String[] args) {
+        getWordsInLongestSubsequence(3,
+                new String[]{"bab", "dab", "cab"},
+                new int[]{1, 2, 2});
+    }
+}
---
+++ leetcode/src/main/java/every_day_topic/compete_100095.java
@@ -0,0 +1,32 @@
+package every_day_topic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SuppressWarnings("unused")
+public class compete_100095 {
+    public List<Integer> lastVisitedIntegers(List<String> words) {
+        List<Integer> ans = new ArrayList<>();
+        List<String> nums = new ArrayList<>();
+        int length = words.size();
+        int numIndex = -1;
+        for (int index = 0; index < length; index++) {
+            if ("prev".equals(words.get(index))) {
+                numIndex--;
+                if (numIndex > -1 && numIndex < nums.size()) {
+                    ans.add(Integer.parseInt(nums.get(numIndex)));
+                } else {
+                    ans.add(-1);
+                }
+            } else {
+                nums.add(words.get(index));
+                numIndex = index;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
