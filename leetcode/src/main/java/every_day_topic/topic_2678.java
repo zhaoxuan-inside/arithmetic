a1ce3a4b1b23d8362782f4cdbd5e22dfb875cd62
zhaoxuan-inside <zhaoxuanmail@163.com>
2023-10-23 13:08:00 +0800

    [Feat] topic_2678

---
+++ leetcode/src/main/java/every_day_topic/topic_2678.java
@@ -0,0 +1,15 @@
+package every_day_topic;
+
+@SuppressWarnings("unused")
+public class topic_2678 {
+    public static int countSeniors(String[] details) {
+        int ans = 0;
+        for (String detail : details) {
+            if (detail.charAt(11) - '6' > 0 ||
+                    (detail.charAt(11) == '6' && detail.charAt(12) - '0' > 0)) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
