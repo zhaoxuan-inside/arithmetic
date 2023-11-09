4c901a4bda4ac1b9129a073409acab7223b70835
zhaoxuan-inside <zhaoxuanmail@163.com>
2023-11-09 13:25:46 +0800

    [Feat] topic_0970

--- leetcode/src/main/java/every_day_topic/topic_0970.java
+++ leetcode/src/main/java/every_day_topic/topic_0970.java
@@ -1,12 +1,10 @@
 package every_day_topic;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
+@SuppressWarnings("unused")
 public class topic_0970 {
-    public static List<Integer> powerfulIntegers(int x, int y, int bound) {
+    public List<Integer> powerfulIntegers(int x, int y, int bound) {
         int min = Math.min(x, y);
         int max = Math.max(x, y);
 
@@ -14,15 +12,12 @@ public class topic_0970 {
 
         for (int lastMin = 1; lastMin < bound; lastMin *= min) {
             for (int lastMax = 1; lastMax + lastMin <= bound; lastMax *= max) {
-               if ((lastMax + lastMin) < bound) {
-                   result.add(lastMax + lastMin);
-               }
+                if ((lastMax + lastMin) < bound) {
+                    result.add(lastMax + lastMin);
+                }
             }
         }
         return new ArrayList<>(result);
     }
 
-    public static void main(String[] args) {
-        powerfulIntegers(3, 5, 15).forEach(System.out::println);
-    }
 }
