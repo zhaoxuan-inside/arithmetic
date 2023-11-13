26bc7fc4c3c1ac95513b6401eec6d441f432bf5a
zhaoxuan-inside <zhaoxuanmail@163.com>
2023-11-13 21:08:19 +0800

    [Feat] topic_0307

---
+++ leetcode/src/main/java/every_day_topic/topic_0307.java
@@ -0,0 +1,25 @@
+package every_day_topic;
+
+@SuppressWarnings("unused")
+public class topic_0307 {
+    class NumArray {
+
+        private int[] nums;
+
+        public NumArray(int[] nums) {
+            this.nums = nums;
+        }
+
+        public void update(int index, int val) {
+            nums[index] = val;
+        }
+
+        public int sumRange(int left, int right) {
+            int sum = 0;
+            for (int index = left; index <= right && index < nums.length; index++) {
+                sum += nums[index];
+            }
+            return sum;
+        }
+    }
+}
