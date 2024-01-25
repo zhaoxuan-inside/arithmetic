876cc272ca4e7eb69b626ccb308952d04234fcd8
zhaoxuan-inside <zhaoxuanmail@163.com>
2024-01-25 13:49:05 +0800

    [Feat] topic_0219

--- leetcode/src/main/java/leetcode/topic_0219.java
+++ leetcode/src/main/java/leetcode/topic_0219.java
@@ -1,14 +1,29 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SuppressWarnings("unused")
 public class topic_0219 {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        for (int start = 0; start < nums.length; start++) {
-            for (int end = start + 1; end < nums.length && end <= start + k; end++) {
-                if (nums[start] == nums[end]) {
-                    return true;
-                }
+        Map<Integer, Integer> WINDOWS = new HashMap<>();
+        for (int idx = 0; idx < k && idx < nums.length; idx++) {
+            Integer count = WINDOWS.get(nums[idx]);
+            if (count == null) {
+                count = 1;
+            } else {
+                return true;
             }
+            WINDOWS.put(nums[idx], count);
+        }
+
+        for (int idx = k; idx < nums.length; idx++) {
+            Integer integer = WINDOWS.get(nums[idx]);
+            if (integer != null) {
+                return true;
+            }
+            WINDOWS.put(nums[idx], 1);
+            WINDOWS.remove(nums[idx - k]);
         }
         return false;
     }
