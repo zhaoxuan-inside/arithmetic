bc3418d3e0fbf689e3aec45eab509835b1371853
zhaoxuan-inside <zhaoxuanmail@163.com>
2024-05-28 14:03:57 +0800

    [Feat] topic_0144

--- leetcode/src/main/java/leetcode/topic_0144.java
+++ leetcode/src/main/java/leetcode/topic_0144.java
@@ -3,17 +3,17 @@ package leetcode;
 import java.util.ArrayList;
 import java.util.List;
 
-public class topic_144 {
+public class topic_0144 {
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
-        inorderTraversal(root, ans);
+        preorder(root, ans);
         return ans;
     }
 
-    public void inorderTraversal(TreeNode root, List<Integer> ans) {
+    public void preorder(TreeNode root, List<Integer> ans) {
         if (root == null) return;
         ans.add(root.val);
-        inorderTraversal(root.left, ans);
-        inorderTraversal(root.right, ans);
+        preorder(root.left, ans);
+        preorder(root.right, ans);
     }
 }
