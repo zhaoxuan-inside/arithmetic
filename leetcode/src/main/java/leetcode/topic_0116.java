725f838c9c51e1befd29dd7ec64fb4b8d52add6c
zhaoxuan-inside <zhaoxuanmail@163.com>
2024-06-03 13:28:54 +0800

    [Feat] topic_0116

--- leetcode/src/main/java/leetcode/topic_0116.java
+++ leetcode/src/main/java/leetcode/topic_0116.java
@@ -4,6 +4,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class topic_0116 {
+
+    private class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+
     public Node connect(Node root) {
         if (root == null) return null;
         List<Node> list = new ArrayList<>();
@@ -29,24 +51,3 @@ public class topic_0116 {
     }
 
 }
-
-class Node {
-    public int val;
-    public Node left;
-    public Node right;
-    public Node next;
-
-    public Node() {
-    }
-
-    public Node(int _val) {
-        val = _val;
-    }
-
-    public Node(int _val, Node _left, Node _right, Node _next) {
-        val = _val;
-        left = _left;
-        right = _right;
-        next = _next;
-    }
-};
