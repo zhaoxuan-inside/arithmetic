6ecb379f80740c63ee9e4cee9702189b7c0ab272
zhaoxuan-inside <zhaoxuanmail@163.com>
2024-06-12 13:15:11 +0800

    [Feat] topic_2806

--- leetcode/src/main/java/leetcode/topic_2806.java
+++ leetcode/src/main/java/leetcode/topic_2806.java
@@ -2,4 +2,7 @@ package leetcode;
 
 @SuppressWarnings("unused")
 public class topic_2806 {
+    public int accountBalanceAfterPurchase(int purchaseAmount) {
+        return 100 - purchaseAmount - (purchaseAmount % 10 >= 5 ? (10 - purchaseAmount % 10) : -1 * purchaseAmount % 10);
+    }
 }
