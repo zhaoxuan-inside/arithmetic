094ddc1f89b2c8ec28020eebb49a18826c6f6440
zhaoxuan <zhaoxuanmail@163.com>
2025-01-07 22:58:12 +0800

    [Feat] 动态规划 简单 0509

--- leetcode/src/main/java/leetcode/topic_0509.java
+++ leetcode/src/main/java/leetcode/topic_0509.java
@@ -2,25 +2,19 @@ package leetcode;
 
 @SuppressWarnings("unused")
 public class topic_0509 {
+
     public static int fib(int n) {
-        if (n == 1) return 1;
-        if (n == 0) return 0;
-        return fib(n - 1) + fib(n - 2);
-    }
 
-    public static int faster(int n) {
-        if (n == 1) return 1;
-        if (n == 0) return 0;
+        int[] steps = new int[3];
+        steps[1] = 1;
 
-        int[] mem = new int[]{0, 1, 1};
-
-        int idx = 2;
-        int count = 2;
-        while (count++ <= n) {
-            mem[idx] = mem[((idx - 1) + 3) % 3] + mem[((idx - 2) + 3) % 3];
-            idx = (idx + 1) % 3;
+        for (int idx = 2; idx <= n; idx++) {
+            // 类似 topic_0070 通过长度为3的数据处理数据
+            steps[idx % 3] = steps[(idx - 1) % 3] + steps[(idx - 2) % 3];
         }
-        return mem[((idx - 1) + 3) % 3];
+
+        return steps[n % 3];
+
     }
 
 }
